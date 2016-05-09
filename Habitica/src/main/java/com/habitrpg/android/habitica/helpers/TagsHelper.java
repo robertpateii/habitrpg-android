fc3f74bf793089a24e0fed2400fb973bfaccc2dc
Robert Pate <robert.pate@gmail.com>
2016-05-09 09:50:51 -0500

    Remove util.log lines from toshevski debugging

--- Habitica/src/main/java/com/habitrpg/android/habitica/helpers/TagsHelper.java
+++ Habitica/src/main/java/com/habitrpg/android/habitica/helpers/TagsHelper.java
@@ -1,6 +1,5 @@
 package com.habitrpg.android.habitica.helpers;
 
-import android.util.Log;
 
 import com.magicmicky.habitrpgwrapper.lib.models.tasks.Task;
 
@@ -54,7 +53,6 @@ public class TagsHelper {
         List<Task> filtered = new ArrayList<Task>();
         for (Task t : tasks) {
             if (t.getType().equals(Task.TYPE_DAILY))
-            Log.i("MT:", "Task is due - " + t.isDisplayedActive(offset) + " N: " + t.getText());
             if (t.isDisplayedActive(offset))
                 filtered.add(t);
         }
